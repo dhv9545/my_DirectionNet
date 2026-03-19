f49fe2c2d8437f4268cb69c809d20b3075cc8207
Dhaval Himmatbhai Variya <dhv9545@thi.de>
2026-03-19 15:02:26 +0100

    removed unwanted file from repo

--- test_vmf.py
+++
@@ -1,9 +0,0 @@
-import math
-import numpy as np
-
-def exact_vmf(kappa, mu, x):
-    C = kappa / (4 * math.pi * math.sinh(kappa))
-    dot = np.dot(mu, x)
-    return C * math.exp(kappa * dot)
-
-print(exact_vmf(10.0, [1,0,0], [1,0,0]))
